8948ad35d494a3e8c7a131261fd8d87b70942f7e
Glitch (the-bot-glitch-2-o-v2) <none>
2024-05-05 08:13:48 +0000

    🐛🎪 Checkpoint ./database.js:51646777/6415 ./package.json:51646777/73

--- database.js
+++ database.js
@@ -24,8 +24,54 @@ import {
 } from "./utils.js";
 import { getShuffledOptions, getResult } from "./game.js";
 
+const userId = `get/users/{user.id}`;
+// Create an express app
+const app = express();
+// Get port, or default to 3000
+const PORT = process.env.PORT || 3000;
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+// Store for in-progress games. In production, you'd want to use a DB
+const activeGames = {};
 
 const userInfo = {
-  User: userId,
-  
-}
+  user: userId,
+  knuts: Number,
+  ytTokens: Number,
+};
+
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ */
+app.post("/interactions", async function (req, res) {
+  // Interaction type and data
+  const { type, id, data } = req.body;
+
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name, options } = data;
+    
+    if ()
+    
+  }
+
+  if (type === InteractionType.MESSAGE_COMPONENT) {
+    // custom_id set in payload when sending message component
+    const componentId = data.custom_id;
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
