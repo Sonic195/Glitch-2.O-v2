ca69eaaf04f6e4f374d4a9b5217ffd495535f6fb
Glitch (the-bot-glitch-2-o-v2) <none>
2024-05-06 13:10:07 +0000

    Rewound to commit 81d4c86cafdb58fb3d89b9121655f7b97e4cf8c3 + 0 edits

--- app.js
+++ app.js
@@ -265,24 +265,45 @@ app.post("/interactions", async function (req, res) {
     // "test" command
     if (name === "test") {
       // embed test
-      const testEmbed = new EmbedBuilder()
+      const testEmbded = new EmbedBuilder()
         .setColour(0x0099ff)
         .setTitle("Test")
         .setURL("https://discord.js.org/")
         .setAuthor({
           name: "Glitch 2.O",
-          icon_URL: "https://i.imgur.com/AfFp7pu.png",
+          iconURL: "https://i.imgur.com/AfFp7pu.png",
           url: "https://discord.js.org",
         })
         .setDescription("testing embed")
         .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+        .addFields(
+          { name: "Regular field title", value: "Some value here" },
+          { name: "\\u200B", value: "\\u200B" },
+          {
+            name: "Inline field title",
+            value: "Some value here",
+            inline: true,
+          },
+          { name: "Inline field title", value: "Some value here", inline: true }
+        )
+        .addFields({
+          name: "Inline field title",
+          value: "Some value here",
+          inline: true,
+        })
+        .setImage("https://i.imgur.com/AfFp7pu.png")
+        .setTimestamp()
+        .setFooter({
+          text: "Some footer text here",
+          iconURL: "https://i.imgur.com/AfFp7pu.png",
+        });
       // Send a message into the channel where command was triggered from
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           // Fetches a random emoji to send from a helper function
           content: "Hello, world! " + getRandomEmoji(),
-          embeds: [testEmbed]
+          embeds: [testEmbed],
         },
       });
     }
