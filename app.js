955e7d707b486d7fffe7f36690572beb10640d5c
Glitch (the-bot-glitch-2-o-v2) <none>
2024-05-06 13:23:44 +0000

    🗻🚲 Checkpoint ./embed.js:51646777/3209 ./app.js:51646777/1165

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ import {
 } from "./utils.js";
 import { getShuffledOptions, getResult } from "./game.js";
 import mongoose from "mongoose";
+import { testEmbed } from "./embed.js";
 
 // Placeholder for an in-memory storage
 // In a real application, you would use a database
@@ -264,39 +265,6 @@ app.post("/interactions", async function (req, res) {
 
     // "test" command
     if (name === "test") {
-      // embed test
-      const testEmbed = new EmbedBuilder()
-        .setColor(0x0099ff)
-        .setTitle("Test")
-        .setURL("https://discord.js.org/")
-        .setAuthor({
-          name: "Glitch 2.O",
-          icon_URL: "https://i.imgur.com/AfFp7pu.png",
-          url: "https://discord.js.org",
-        })
-        .setDescription("testing embed")
-        .setThumbnail("https://i.imgur.com/AfFp7pu.png")
-        .addFields(
-          { name: "Regular field title", value: "Some value here" },
-          { name: "\\u200B", value: "\\u200B" },
-          {
-            name: "Inline field title",
-            value: "Some value here",
-            inline: true,
-          },
-          { name: "Inline field title", value: "Some value here", inline: true }
-        )
-        .addFields({
-          name: "Inline field title",
-          value: "Some value here",
-          inline: true,
-        })
-        .setImage("https://i.imgur.com/AfFp7pu.png")
-        .setTimestamp()
-        .setFooter({
-          text: "Some footer text here",
-          iconURL: "https://i.imgur.com/AfFp7pu.png",
-        });
       // Send a message into the channel where command was triggered from
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
---
+++ embed.js
@@ -0,0 +1,34 @@
+import { EmbedBuilder } from "discord.js";
+
+ export const testEmbed = new EmbedBuilder()
+  .setColor(0x0099ff)
+  .setTitle("Test")
+  .setURL("https://discord.js.org/")
+  .setAuthor({
+    name: "Glitch 2.O",
+    icon_URL: "https://i.imgur.com/AfFp7pu.png",
+    url: "https://discord.js.org",
+  })
+  .setDescription("testing embed")
+  .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+  .addFields(
+    { name: "Regular field title", value: "Some value here" },
+    { name: "\\u200B", value: "\\u200B" },
+    {
+      name: "Inline field title",
+      value: "Some value here",
+      inline: true,
+    },
+    { name: "Inline field title", value: "Some value here", inline: true }
+  )
+  .addFields({
+    name: "Inline field title",
+    value: "Some value here",
+    inline: true,
+  })
+  .setImage("https://i.imgur.com/AfFp7pu.png")
+  .setTimestamp()
+  .setFooter({
+    text: "Some footer text here",
+    iconURL: "https://i.imgur.com/AfFp7pu.png",
+  });
