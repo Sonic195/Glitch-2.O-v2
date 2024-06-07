fb4c65e2012b9962f3488508c9f8545fa357ce75
Glitch (the-bot-glitch-2-o-v2) <none>
2024-06-07 09:31:46 +0000

    🚠🐂 Checkpoint ./app.js:51646777/2398

--- app.js
+++ app.js
@@ -211,7 +211,7 @@ app.post("/interactions", async function (req, res) {
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
               content: `https://www.youtube.com/watch?v=${videoId}`,
-              embeds: [ytEmbed],
+              
               components: [row],
             },
           });
@@ -409,7 +409,7 @@ app.post("/interactions", async function (req, res) {
    */
   if (type === InteractionType.MESSAGE_COMPONENT) {
     // custom_id set in payload when sending message component
-    const componentId = data.custom_id;
+    const { componentId, options } = data.custom_id;
 
     if (componentId.startsWith("nextVid")) {
       const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
@@ -444,7 +444,16 @@ app.post("/interactions", async function (req, res) {
           },
         });
         }
+      } catch (error) {
+        console.error("Error fetching from youtube:", error);
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: "An error occurred while fetching the image.",
+          },
+        });
       }
+      
     }
 
     if (componentId.startsWith("ping_pong_button")) {
