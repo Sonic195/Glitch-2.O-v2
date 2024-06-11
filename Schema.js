2fde166bf19bbeb0068e3bc7b59bc31154cc0f1c
Glitch (the-bot-glitch-2-o-v2) <none>
2024-06-11 08:39:41 +0000

    🐇😜 Checkpoint ./Schema.js:51646777/319

--- Schema.js
+++ Schema.js
@@ -1,3 +1,18 @@
 const { Schema, model } = require('mongoose');
 
-const level
+const auraSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  aura: {
+    type: Number,
+    default: 0,
+  },
+  level: {
+    type: Number,
+    default: 0,
+  }
+})
+
+module.exports = model('Aura', auraSchema);
